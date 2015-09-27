6c3155ceb8bbd04c041171c59466282d45df50f3
A.Kisiel -ALEXANDER-WS <akisiel@outlook.com>
2015-09-27 15:08:56 +0100

    Initial Commit

---
+++ ToBeNamed.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ToBeNamed.Tests
+{
+    [TestClass]
+    public class AlgorithmTests
+    {
+        [TestMethod]
+        public void TestAlgorithmRetrieval()
+        {
+        }
+    }
+}
